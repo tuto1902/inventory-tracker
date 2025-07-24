8d5bbca2579360dbda4dead1bffc8e1946384c02
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2025-07-23 20:58:37 -0600

    Update products and suppliers relationship to many-to-many, modify products table schema

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
@@ -20,8 +22,8 @@ class Product extends Model
         'supplier_id',
     ];
 
-    public function supplier(): BelongsTo
+    public function suppliers(): BelongsToMany
     {
-        return $this->belongsTo(Supplier::class);
+        return $this->belongsToMany(Supplier::class);
     }
 }
